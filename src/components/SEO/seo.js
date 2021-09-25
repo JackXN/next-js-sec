22a29529c44cd986b2fb8f7ed844081f0f8e25eb
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-25 16:56:14 -0600

    Implemented seo.js for SEO gains

---
+++ src/components/SEO/seo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Head from 'next/head';
+
+
+const SEO = ({
+description = 'Apex security is a fictional cyber security company',
+author='Jack Rigan',
+meta,
+title='Apex Security'
+}) => {
+const metaData = [
+    {
+        name: `description`,
+        content: description,
+      },
+      {
+        property: `og:title`,
+        content: title,
+      },
+      {
+        property: `og:description`,
+        content: description,
+      },
+      {
+        property: `og:type`,
+        content: `website`,
+      },
+];
+
+
+return (
+    <Head>
+        <title>{title}</title>
+        {metaData.map(({name,content}, i) => (
+            <meta key={index} name={name} content={content}/>
+        ))}
+    </Head>
+)
+
+}
